f821abd38217de4f958bdc385cca9752a1ce7a45
Leon <115641341+leonasanovski@users.noreply.github.com>
2024-12-17 15:52:57 +0100

    lab4
    labaratoriska 4 - grupa A

--- labaratoriska vezba 1 - grupaA/src/main/java/mk/finki/ukim/mk/lab/LabaratoriskaVezba1GrupaAApplication.java
+++ labaratoriska vezba 1 - grupaA/src/main/java/mk/finki/ukim/mk/lab/LabaratoriskaVezba1GrupaAApplication.java
@@ -3,7 +3,10 @@ package mk.finki.ukim.mk.lab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class LabaratoriskaVezba1GrupaAApplication {
@@ -11,5 +14,8 @@ public class LabaratoriskaVezba1GrupaAApplication {
     public static void main(String[] args) {
         SpringApplication.run(LabaratoriskaVezba1GrupaAApplication.class, args);
     }
-
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 }
